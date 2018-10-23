7fd724c871237b15694e058e1cd81b4a10697a31
Anton Vishneuski <wishell@macbook-pro-wishell.local>
2018-10-23 23:54:36 +0300

    - switch applicationIconBadgeNumber in different application states

--- Hw_first/Hw_first/AppDelegate.swift
+++ Hw_first/Hw_first/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+var state: Array<String> = []
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        state.append("didFinishLaunchingWithOptions")
         // Override point for customization after application launch.
         return true
     }
@@ -22,25 +24,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        UIApplication.shared.applicationIconBadgeNumber = 3
+        state.append("applicationWillResignActive")
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        UIApplication.shared.applicationIconBadgeNumber = 10
+        state.append("applicationDidEnterBackground")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+         state.append("applicationWillEnterForeground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        UIApplication.shared.applicationIconBadgeNumber = 0
+        state.append("applicationDidBecomeActive")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        state.append("applicationWillTerminate")
+        state.forEach({(currState) in print("application state: \(currState)")})
     }
-
 
 }
 
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var state: Array<String> = ["1","2","3","4","5","6","7"]
+state.forEach({(currState) in print("application state: \(currState)")})
