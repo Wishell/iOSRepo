ec2c95d9c8b2aad05f47dd6bf700c7d4e6fbe7d9
Anton Vishneuski <wishell@macbook-pro-wishell.local>
2018-10-24 23:41:04 +0300

    -added some outlets and storyboard elements, implemented ViewController life circle methods

--- Hw_first/Hw_first/AppDelegate.swift
+++ Hw_first/Hw_first/AppDelegate.swift
@@ -7,7 +7,13 @@
 //
 
 import UIKit
-var state: Array<String> = []
+enum state {
+    case inactive
+    case active
+    case background
+    case unknown
+}
+var logState: Array<state> = []
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +22,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        state.append("didFinishLaunchingWithOptions")
         // Override point for customization after application launch.
         return true
     }
@@ -25,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         UIApplication.shared.applicationIconBadgeNumber = 3
-        state.append("applicationWillResignActive")
+        logState.append(.background)
         
     }
 
@@ -34,24 +39,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         
         UIApplication.shared.applicationIconBadgeNumber = 10
-        state.append("applicationDidEnterBackground")
+        logState.append(.background)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-         state.append("applicationWillEnterForeground")
+         logState.append(.active)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         UIApplication.shared.applicationIconBadgeNumber = 0
-        state.append("applicationDidBecomeActive")
+        logState.append(.active)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        state.append("applicationWillTerminate")
-        state.forEach({(currState) in print("application state: \(currState)")})
+        logState.forEach({(currState) in print("application state: \(currState)")})
     }
 
 }
