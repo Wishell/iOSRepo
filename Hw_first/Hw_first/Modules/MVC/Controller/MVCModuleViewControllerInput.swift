f68ee9f19d4102a3b2c9a255836031543ea414cb
Wishell <wishell@bk.ru>
2018-10-26 22:11:01 +0300

    - clean project

--- Hw_first/Hw_first/Modules/MVC/Controller/MVCModuleViewControllerInput.swift
+++
@@ -1,9 +0,0 @@
-//  
-//  MVCModuleViewControllerInput.swift
-//  Hw_first
-//
-//  Created by Wishell on 25.10.2018.
-//  Copyright © 2018 Wishell. All rights reserved.
-//
-
-protocol MVCModuleViewControllerInput: class {}
