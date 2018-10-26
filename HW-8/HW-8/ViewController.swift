422e80c37d20975243944ecdd4d2ffa5aa48e95e
Wishell <wishell@bk.ru>
2018-10-26 22:13:51 +0300

    -create new project

---
+++ HW-8/HW-8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW-8
+//
+//  Created by Wishell on 26.10.2018.
+//  Copyright © 2018 Wishell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
