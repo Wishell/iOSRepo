ebfb33968855d6f20a67916f4102a46dc82f4643
Wishell <wishell@bk.ru>
2018-11-09 14:12:12 +0300

    - new branch

--- ImaguruClassWork/ImaguruClassWork/Modules/About/Controller/AboutViewControllerInput.swift
+++
@@ -1,9 +0,0 @@
-//  
-//  AboutViewControllerInput.swift
-//  ImaguruClassWork
-//
-//  Created by Wishell on 03.11.2018.
-//  Copyright © 2018 Wishell. All rights reserved.
-//
-
-protocol AboutViewControllerInput: class {}
