53d08f957415e4470a69f8524ad52049946fde5f
Wishell <wishell@bk.ru>
2018-11-27 20:21:54 +0300

    - added gitignore

---
+++ HW-15/HW-15Tests/HW_15Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HW_15Tests.swift
+//  HW-15Tests
+//
+//  Created by Wishell on 25.11.2018.
+//  Copyright © 2018 Wishell. All rights reserved.
+//
+
+import XCTest
+@testable import HW_15
+
+class HW_15Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
